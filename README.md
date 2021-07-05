c7b7392182544b1d861c17959fd312f301332da0
Byron Marohn <Combustible@live.com>
2021-07-05 00:53:39 +0000

    Initial commit
    Signed-off-by: Byron Marohn <Combustible@live.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# monumenta-world-management
+
+Bungeecord & network aware world management / instancing system. Depends on MonumentaNetworkRelay and MonumentaRedisSync.
---
+++ src/main/java/com/playmonumenta/worlds/WorldManagementPlugin.java
@@ -0,0 +1,21 @@
+package com.playmonumenta.worlds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WorldManagementPlugin extends JavaPlugin {
+
+	@Override
+	public void onLoad() {
+
+	}
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
