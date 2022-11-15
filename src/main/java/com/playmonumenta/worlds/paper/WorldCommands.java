9c53668105e994460a8bd21cd842c49749b128aa
Ziggleflig <dmdinnc@gmail.com>
2022-11-14 23:07:50 -0500

    tptoworld: style

--- src/main/java/com/playmonumenta/worlds/paper/WorldCommands.java
+++ src/main/java/com/playmonumenta/worlds/paper/WorldCommands.java
@@ -13,7 +13,12 @@ import dev.jorel.commandapi.arguments.LocationType;
 import dev.jorel.commandapi.arguments.StringArgument;
 import dev.jorel.commandapi.exceptions.WrapperCommandSyntaxException;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
