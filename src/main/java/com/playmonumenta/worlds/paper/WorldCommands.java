b32ff8708bfb2ad7f3216e64478f1b398442216b
Ziggleflig <dmdinnc@gmail.com>
2022-11-14 23:18:54 -0500

    tptoworld: style again

--- src/main/java/com/playmonumenta/worlds/paper/WorldCommands.java
+++ src/main/java/com/playmonumenta/worlds/paper/WorldCommands.java
@@ -15,6 +15,7 @@ import dev.jorel.commandapi.exceptions.WrapperCommandSyntaxException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
