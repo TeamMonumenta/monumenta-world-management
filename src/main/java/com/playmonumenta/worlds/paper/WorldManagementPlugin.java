f3693fe39b82b358eabe6df755d50134021488fb
Byron Marohn <Combustible@live.com>
2023-01-13 02:39:13 +0000

    Revert CommandAPI shading
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- src/main/java/com/playmonumenta/worlds/paper/WorldManagementPlugin.java
+++ src/main/java/com/playmonumenta/worlds/paper/WorldManagementPlugin.java
@@ -1,8 +1,6 @@
 package com.playmonumenta.worlds.paper;
 
 import com.playmonumenta.worlds.common.CustomLogger;
-import dev.jorel.commandapi.CommandAPI;
-import dev.jorel.commandapi.CommandAPIConfig;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -38,23 +36,12 @@ public class WorldManagementPlugin extends JavaPlugin {
 
 	@Override
 	public void onLoad() {
-		// Load the CommandAPI. We enable verbose logging and allow the CommandAPI
-		// to generate a file command_registration.json for debugging purposes
-		CommandAPI.onLoad(
-			new CommandAPIConfig()
-				.verboseOutput(true)
-				.dispatcherFile(new File(getDataFolder(), "worldmanagement_command_registration.json"))
-		);
-
 		WorldCommands.register(this);
 	}
 
 	@Override
 	public void onEnable() {
 		INSTANCE = this;
-
-		// Enable the CommandAPI
-		CommandAPI.onEnable(this);
 
 		loadConfig();
 
