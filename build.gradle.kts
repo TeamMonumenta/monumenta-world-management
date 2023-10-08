79fc9723b912f2927db57b14466d35fa07c2bfdd
Timothy Southwick <NickNackGus@gmail.com>
2023-10-08 16:29:48 -0400

    Added volt-deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -170,6 +170,19 @@ tasks.create("stage-deploy") {
     }
 }
 
+tasks.create("volt-deploy") {
+    val shadowJar by tasks.named<ShadowJar>("shadowJar")
+    dependsOn(shadowJar)
+    doLast {
+        ssh.runSessions {
+            session(basicssh) {
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/volt/m12/server_config/plugins")
+                execute("cd /home/epic/volt/m12/server_config/plugins && rm -f MonumentaWorldManagement.jar && ln -s " + shadowJar.archiveFileName.get() + " MonumentaWorldManagement.jar")
+            }
+        }
+    }
+}
+
 tasks.create("build-deploy") {
     val shadowJar by tasks.named<ShadowJar>("shadowJar")
     dependsOn(shadowJar)
